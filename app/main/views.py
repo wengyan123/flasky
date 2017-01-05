9ecf6d164a83b63e622d6495670684167b86fa23
wengyan <wengyan123@gmail.com>
2017-01-05 13:18:16 +0800

    10b

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from flask import render_template, abort
+from flask import render_template, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 from . import main
-from ..models import User
+from .forms import EditProfileAdminForm, EditProfileForm
+from .. import db
+from ..models import User, Role
+from ..decorators import admin_required
 
 
 @main.route('/')
@@ -12,3 +16,21 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
