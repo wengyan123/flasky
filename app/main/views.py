fc25f9e51ed5c144bc56002e76d06b94f8c65789
wengyan <wengyan123@gmail.com>
2017-01-26 11:29:05 +0800

    12e

--- app/main/views.py
+++ app/main/views.py
@@ -25,12 +25,12 @@ def index():
         query = current_user.followed_posts
     else:
         query = Post.query
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, show_followed=show_followed, pagination=pagination)
-
+    return render_template('index.html', form=form, posts=posts,
+                           show_followed=show_followed, pagination=pagination)
 
 
 @main.route('/user/<username>')
